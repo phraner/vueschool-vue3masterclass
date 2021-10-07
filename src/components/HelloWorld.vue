67c18a4120072119ee65dee57209ed4131486251
phraner <phraneralmos@gmail.com>
2021-10-07 15:31:12 +0200

    Displaying a List of Elements

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -27,6 +27,7 @@
       <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
     </ul>
+    <div>{{threads}}</div>
   </div>
 </template>
 
@@ -39,6 +40,11 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data () {
+    return {
+      threads: sourceData.threads
+    }
   }
 }
 </script>
